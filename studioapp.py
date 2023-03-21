52a4176954636da23ad4449a66b889cba5e2c760
Lachlan Greig <lachlanrgreig@gmail.com>
2023-03-21 23:45:35 +1100

    changes

---
+++ studioapp.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# this is where I can add more html pages
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/student/')
+def student():
+    return render_template('student.html')
+
+@app.route('/profile/')
+def profile():
+    return render_template('profile.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
